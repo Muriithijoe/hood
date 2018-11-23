9a180108926ae8f9ea56f228a8210c5bc032c6fb
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2018-11-23 12:11:48 +0300

    added models to admin

--- updateapp/admin.py
+++ updateapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Neighborhood, User, Business
 
-# Register your models here.
+admin.site.register(Neighborhood)
+admin.site.register(User)
+admin.site.register(Business)
