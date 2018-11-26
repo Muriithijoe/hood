78e3779c220bb9856d6d140f91728f540a99ebce
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2018-11-26 11:23:01 +0300

    debugged

--- updateapp/admin.py
+++ updateapp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Neighborhood, User, Business, Update
+from .models import Neighborhood,UserProfile,Business,Update,Health,Police
 
 admin.site.register(Neighborhood)
-admin.site.register(User)
+admin.site.register(UserProfile)
 admin.site.register(Business)
 admin.site.register(Update)
+admin.site.register(Health)
+admin.site.register(Police)
