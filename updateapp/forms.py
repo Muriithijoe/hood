fa552c3270b736c40d52b330a3efd5ba5ef6a13b
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2018-11-26 10:41:29 +0300

    made foms

--- updateapp/forms.py
+++ updateapp/forms.py
@@ -1,11 +1,27 @@
 from django import forms
-from .models import Neighborhood, User, Business, Update
+from .models import User,Neighborhood,Business,Update,Health
 
 class UpdateForm(forms.ModelForm):
     class Meta:
         model = Update
+        exclude = ['writer']
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        fields = '__all__'
+
+class HealthForm(forms.ModelForm):
+    class Meta:
+        model = Health
+        fields = '__all__'
 
 class ProfileForm(forms.ModelForm):
     class Meta:
-        model = User
-        
+        model = UserStatus
+        exclude = ['user']
+
+class NeighborhoodForm(forms.ModelForm):
+    class Meta:
+        model = UserStatus
+        exclude = ['user','user_image','user_email']
